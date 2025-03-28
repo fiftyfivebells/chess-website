a899b5536844b9f5c8b695e3d14213498478358c
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-03-28 14:24:07 -0400

    Change ChessService endpoints to take Option[String]s

--- src/main/scala/com/ffb/chess/service/ChessService.scala
+++ src/main/scala/com/ffb/chess/service/ChessService.scala
@@ -2,19 +2,23 @@ package com.ffb.chess.service
 
 import cats.effect.IO
 
-import com.ffb.chess.engine.{ChessEngine, ChessEngineClient, GoEngine}
+import com.ffb.chess.engine.{ChessEngine, ChessEngineClient}
 import com.ffb.chess.domain.Move
 
 object ChessService {
   def bestMove(
       fen: Option[String],
-      engine: ChessEngine = GoEngine
-  ): IO[Move] =
+      engineName: Option[String]
+  ): IO[Move] = {
+    val engine = ChessEngine.fromOption(engineName)
     ChessEngineClient.create(engine).use(_.bestMove(fen))
+  }
 
   def allMoves(
       fen: Option[String],
-      engine: ChessEngine = GoEngine
-  ): IO[Seq[Move]] =
+      engineName: Option[String]
+  ): IO[Seq[Move]] = {
+    val engine = ChessEngine.fromOption(engineName)
     ChessEngineClient.create(engine).use(_.allMoves(fen))
+  }
 }
