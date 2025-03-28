1222cc54dceeb143d3f71468bcfac048ae4a3b27
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-03-28 14:23:18 -0400

    Add helpers to create new ChessEngine objects

--- src/main/scala/com/ffb/chess/engine/ChessEngine.scala
+++ src/main/scala/com/ffb/chess/engine/ChessEngine.scala
@@ -4,6 +4,23 @@ sealed trait ChessEngine {
   def engineName: String
 }
 
+object ChessEngine {
+  val defaultEngine: ChessEngine = GoEngine
+
+  def fromString(engineName: String): ChessEngine = engineName match {
+    // this will eventually choose the engine based on the given engine name or
+    // default to some engine if the names don't match, but there's only one engine
+    // right now, so default to Go.
+    case "go" => GoEngine
+    case _    => defaultEngine
+  }
+
+  def fromOption(engineName: Option[String]): ChessEngine = engineName match {
+    case None => defaultEngine
+    case Some(name) => fromString(name)
+  }
+}
+
 final case object GoEngine extends ChessEngine {
   override val engineName: String = "./engines/nsdb-go-edition"
 }
