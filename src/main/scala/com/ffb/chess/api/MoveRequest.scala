e626fcf734c6d28f7056612daa22fb8ab9757ef1
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-03-29 13:27:10 -0400

    Add moves to the MoveRequest object

--- src/main/scala/com/ffb/chess/api/MoveRequest.scala
+++ src/main/scala/com/ffb/chess/api/MoveRequest.scala
@@ -2,8 +2,9 @@ package com.ffb.chess.api
 
 import io.circe.Decoder
 import io.circe.generic.semiauto._
+import com.ffb.chess.domain.Move
 
-final case class MoveRequest(position: Option[String], engine: Option[String])
+final case class MoveRequest(position: Option[String], moves: Option[Seq[Move]], engine: Option[String])
 
 object MoveRequest {
 //  implicit val encoder: Encoder[MoveRequest] = deriveEncoder[MoveRequest]
