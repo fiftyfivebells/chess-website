141ffe1b32aa957b0ec187e2cfb607fefb80bf3d
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-03-31 18:39:50 -0400

    Update piece representation for easier comparisons

--- front-end/src/utils/ChessRules.ts
+++ front-end/src/utils/ChessRules.ts
@@ -2,19 +2,13 @@ export type Color = "white" | "black";
 export const WHITE: Color = "white";
 export const BLACK: Color = "black";
 
-export type PieceType =
-  | "pawn"
-  | "knight"
-  | "bishop"
-  | "rook"
-  | "queen"
-  | "king";
-export const PAWN: PieceType = "pawn";
-export const KNIGHT: PieceType = "knight";
-export const BISHOP: PieceType = "bishop";
-export const ROOK: PieceType = "rook";
-export const QUEEN: PieceType = "queen";
-export const KING: PieceType = "king";
+export type PieceType = "p" | "n" | "b" | "r" | "q" | "k";
+export const PAWN: PieceType = "p";
+export const KNIGHT: PieceType = "n";
+export const BISHOP: PieceType = "b";
+export const ROOK: PieceType = "r";
+export const QUEEN: PieceType = "q";
+export const KING: PieceType = "k";
 
 export type Piece = {
   color: Color;
