5216b6120d56580149cab2f797e479244ac34262
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-06-07 22:19:14 -0400

    Make stubs for all the endpoints

--- src/main/scala/com/ffb/chess/api/ChessRoutes.scala
+++ src/main/scala/com/ffb/chess/api/ChessRoutes.scala
@@ -1,17 +1,18 @@
 package com.ffb.chess.routes
 
-import org.http4s.{EntityEncoder, HttpRoutes}
-import org.http4s.dsl.Http4sDsl
-import org.http4s.circe._
-import io.circe.generic.auto._
 import cats.effect.IO
+import io.circe.syntax.*
+import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes}
+import org.http4s.dsl.Http4sDsl
+import org.http4s.circe.*
 
+import com.ffb.chess.api.MoveRequest
 import com.ffb.chess.domain.Move
 import com.ffb.chess.service.ChessService
-import org.http4s.EntityDecoder
-import com.ffb.chess.api.MoveRequest
+import com.ffb.chess.service.ChessService.bestMove
+import com.ffb.zugzwang.chess.GameState
 
-object ChessRoutes extends Http4sDsl[IO] {
+object ChessRoutes extends Http4sDsl[IO]:
   implicit val moveEncoder: EntityEncoder[IO, Move] =
     jsonEncoderOf[IO, Move]
 
@@ -22,15 +23,29 @@ object ChessRoutes extends Http4sDsl[IO] {
     jsonOf[IO, MoveRequest]
 
   def routes(): HttpRoutes[IO] =
-    HttpRoutes.of[IO] { case req @ POST -> Root / "bestmove" =>
-      for {
-        moveReq <- req.as[MoveRequest]
-        bestMove <- ChessService.bestMove(
-          moveReq.position,
-          moveReq.moves,
-          moveReq.engine
-        )
-        response <- Ok(bestMove)
-      } yield response
+    HttpRoutes.of[IO] {
+      case GET -> Root / "new" => ???
+
+      case GET -> Root / id / "state" => ???
+
+      case GET -> Root / id / "legalMoves" => ???
+
+      case req @ POST -> Root / id / "applyMove" => ???
+
+      case req @ POST -> Root / id / "bestmove" =>
+        for {
+          moveReq <- req.as[MoveRequest]
+          bestMove <- ChessService.bestMove(
+            moveReq.position,
+            moveReq.moves,
+            moveReq.engine
+          )
+          response <- Ok(bestMove)
+        } yield response
+
+      case GET -> Root / "ping" =>
+        val initFen = GameState.initialFEN
+
+        Ok(Map("initialFen" -> initFen).asJson)
+
     }
-}
