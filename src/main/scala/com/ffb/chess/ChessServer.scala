6953f447a138ecea29b2b7d5ee9ef7b3f2a7c4d2
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-06-22 18:32:25 -0400

    Modify the server to pass in a repo instance

--- src/main/scala/com/ffb/chess/ChessServer.scala
+++ src/main/scala/com/ffb/chess/ChessServer.scala
@@ -1,26 +1,30 @@
 package com.ffb.chess
 
 import cats.effect.IO
+import cats.effect.IO.asyncForIO
 import com.comcast.ip4s.{Host, Port}
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.HttpRoutes
-import org.http4s.implicits._
+import org.http4s.implicits.*
 import org.http4s.server.Router
 
 import com.ffb.chess.routes.ChessRoutes
+import com.ffb.chess.repository.ChessGameRepository
 
 object Server {
-  def routes: HttpRoutes[IO] = Router(
-    "/api/chess/game" -> ChessRoutes.routes()
-  )
 
-  def runServer(): IO[Unit] = {
-    EmberServerBuilder
-      .default[IO]
-      .withHost(Host.fromString("localhost").get)
-      .withPort(Port.fromInt(8080).get)
-      .withHttpApp(routes.orNotFound)
-      .build
-      .use(_ => IO.never) // Keep the server running
-  }
+  def runServer: IO[Unit] =
+    for {
+      repo <- ChessGameRepository.create[IO]
+      routes = Router(
+        "/api/chess/game" -> ChessRoutes.routes(repo)
+      ).orNotFound
+      _ <- EmberServerBuilder
+        .default[IO]
+        .withHost(Host.fromString("localhost").get)
+        .withPort(Port.fromInt(8080).get)
+        .withHttpApp(routes)
+        .build
+        .use(_ => IO.never) // Keep the server running
+    } yield ()
 }
