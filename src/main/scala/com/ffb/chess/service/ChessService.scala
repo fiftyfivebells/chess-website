a9cdc0e13f36cfa2d6004d8066eab08ddf991607
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-06-26 21:40:01 -0400

    Add helper functions to do the chess related logic

--- src/main/scala/com/ffb/chess/service/ChessService.scala
+++ src/main/scala/com/ffb/chess/service/ChessService.scala
@@ -5,11 +5,21 @@ import cats.effect.IO
 import com.ffb.chess.domain.PositionDetails
 import com.ffb.chess.engine.{ChessEngine, ChessEngineClient}
 import com.ffb.zugzwang.move.Move
+import com.ffb.zugzwang.notation.FENParserError
 import com.ffb.zugzwang.Zugzwang
+
 
 object ChessService:
   def newGame: PositionDetails =
     PositionDetails.from(Zugzwang.initial)
+
+  def legalMoves(position: PositionDetails): Either[FENParserError, List[Move]] =
+    Zugzwang.legalMoves(position.fen).map(_.toList)
+
+  def applyMove(position: PositionDetails, move: Move): Either[FENParserError, PositionDetails] =
+    Zugzwang.applyMove(position.fen, move) map { gameState =>
+      PositionDetails.from(gameState)
+    }
 
   def bestMove(
     fen: Option[String],
