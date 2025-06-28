2d0ee0d46403d82ab70d12faba621009ef902fc4
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-06-27 21:58:52 -0400

    Make moves an optional field

--- src/main/scala/com/ffb/chess/engine/UciCommands.scala
+++ src/main/scala/com/ffb/chess/engine/UciCommands.scala
@@ -1,28 +1,20 @@
 package com.ffb.chess.engine
 
-sealed trait UciCommand {
+sealed trait UciCommand:
   def command: String
-}
 
-case object Uci extends UciCommand {
+case object Uci extends UciCommand:
   override val command: String = "uci"
-}
 
-case object UciNewGame extends UciCommand {
+case object UciNewGame extends UciCommand:
   override val command: String = "ucinewgame"
-}
 
-case class Position(fen: Option[String], moves: String)
-    extends UciCommand {
-  override val command: String = fen match {
+case class Position(fen: Option[String], moves: Option[String]) extends UciCommand:
+  override val command: String = fen match
     case Some(f) => s"position fen $f $moves"
     case None    => s"position startpos $moves"
-  }
-}
 
-case class Go(args: String*) extends UciCommand {
-  override val command: String = args match {
+case class Go(args: String*) extends UciCommand:
+  override val command: String = args match
     case Nil => "go"
     case lst => s"go ${lst.mkString(" ")}"
-  }
-}
